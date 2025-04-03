3889a66f60eb0970510d1095c4c42b1c2812a351
developerOlive <chloehiho@gmail.com>
2025-04-04 03:14:41 +0900

    test: E2E 테스트를 위한 RestAssured 및 관련 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,18 @@ dependencies {
     annotationProcessor(libs.lombok)
     annotationProcessor(libs.spring.boot.configuration.processor)
     testImplementation(libs.spring.boot.starter.test)
+
+    // Docs
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+
+    testImplementation("io.rest-assured:rest-assured:5.4.0")
+    testImplementation("io.rest-assured:json-path:5.4.0")
+    testImplementation("io.rest-assured:json-schema-validator:5.4.0")
 }
 
 // about source and compilation
@@ -57,3 +68,6 @@ tasks.test {
     useJUnitPlatform()
 }
 
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-parameters")
+}
