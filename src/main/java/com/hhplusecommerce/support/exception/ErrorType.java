298720e17f5630cfff64689e9caa0d0c2e2d970e
developerOlive <chloehiho@gmail.com>
2025-04-10 22:49:17 +0900

    fix: ErrorType 내용 추가

--- src/main/java/com/hhplusecommerce/support/exception/ErrorType.java
+++ src/main/java/com/hhplusecommerce/support/exception/ErrorType.java
@@ -6,6 +6,7 @@ public enum ErrorType {
     // Product
     INSUFFICIENT_STOCK(HttpStatus.BAD_REQUEST, "상품의 재고가 부족합니다."),
     INVALID_STOCK_AMOUNT(HttpStatus.BAD_REQUEST, "수량은 1 이상이어야 합니다."),
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "상품이 존재하지 않습니다."),
 
     // Balance
     INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
@@ -18,8 +19,21 @@ public enum ErrorType {
     COUPON_INACTIVE(HttpStatus.BAD_REQUEST, "사용할 수 없는 쿠폰입니다."),
     INVALID_COUPON_VALUE(HttpStatus.BAD_REQUEST, "유효하지 않은 쿠폰 할인 값입니다."),
     COUPON_ISSUE_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "쿠폰 발급 수량을 초과했습니다."),
-    COUPON_ALREADY_ISSUED(HttpStatus.CONFLICT, "이미 발급된 쿠폰입니다.");
+    COUPON_ALREADY_ISSUED(HttpStatus.CONFLICT, "이미 발급된 쿠폰입니다."),
+    COUPON_ALREADY_USED(HttpStatus.CONFLICT, "이미 사용된 쿠폰입니다."),
+    UNAUTHORIZED_COUPON_ACCESS(HttpStatus.FORBIDDEN, "해당 쿠폰에 접근할 수 없습니다."),
 
+    // Order
+    ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "주문이 존재하지 않습니다."),
+    INVALID_ORDER_STATUS_TO_COMPLETE(HttpStatus.BAD_REQUEST, "결제 대기 상태에서만 주문을 완료할 수 있습니다."),
+    INVALID_ORDER_STATUS_TO_CANCEL(HttpStatus.BAD_REQUEST, "결제 대기 상태에서만 주문을 취소할 수 있습니다."),
+    INVALID_ORDER_STATUS_TO_EXPIRE(HttpStatus.BAD_REQUEST, "결제 대기 상태에서만 주문을 만료처리 할 수 있습니다."),
+
+    // Payment
+    PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "결제 정보가 존재하지 않습니다."),
+    INVALID_PAYMENT_AMOUNT(HttpStatus.BAD_REQUEST, "결제 금액은 0 이상이어야 합니다."),
+    INVALID_PAYMENT_STATUS_TO_COMPLETE(HttpStatus.BAD_REQUEST, "PENDING 상태에서 완료 처리할 수 있습니다."),
+    INVALID_PAYMENT_STATUS_TO_FAIL(HttpStatus.BAD_REQUEST, "PENDING 상태에서 실패 처리할 수 있습니다.")
     ;
 
     private final HttpStatus status;
