5c7617eea08b28e180bd3b61846b4da1b316b70d
developerOlive <chloehiho@gmail.com>
2025-04-25 02:29:55 +0900

    feat: 요청 로그 및 트래킹을 위한 MDC 필터 및 logback 설정 추가

--- src/main/java/com/hhplusecommerce/infrastructure/order/OrderRepositoryImpl.java
+++ src/main/java/com/hhplusecommerce/infrastructure/order/OrderRepositoryImpl.java
@@ -32,7 +32,7 @@ public class OrderRepositoryImpl implements OrderRepository {
     }
 
     @Override
-    public List<Order> findByStatusAndCreatedAtBefore(OrderStatus status, LocalDateTime time) {
-        return orderJpaRepository.findByOrderStatusAndCreatedAtBefore(status, time);
+    public List<Order> findExpiredOrders(OrderStatus status, LocalDateTime from, LocalDateTime to) {
+        return orderJpaRepository.findByOrderStatusAndCreatedAtBetween(status, from, to);
     }
 }
