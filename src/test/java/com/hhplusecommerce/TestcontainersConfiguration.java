8eafc012d84f2c302af97878213bc9da71158c5a
developerOlive <chloehiho@gmail.com>
2025-04-24 22:37:26 +0900

    fix: @Container 적용으로 테스트마다 생성되던 MySQL 컨테이너 중복 문제 해결

--- src/test/java/com/hhplusecommerce/TestcontainersConfiguration.java
+++ src/test/java/com/hhplusecommerce/TestcontainersConfiguration.java
@@ -1,38 +1,34 @@
 package com.hhplusecommerce;
 
-import jakarta.annotation.PreDestroy;
-import org.springframework.context.annotation.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Configuration
-public class TestcontainersConfiguration {
+@SpringBootTest
+@ActiveProfiles("test")
+@Testcontainers
+public abstract class TestcontainersConfiguration {
 
-    private static final int MYSQL_PORT = 3306;
-
-    public static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+    @Container
+    private static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>("mysql:8.0")
             .withDatabaseName("hhplus")
-            .withExposedPorts(MYSQL_PORT)
             .withUsername("test")
             .withPassword("test");
 
-    static {
-        MYSQL_CONTAINER.start();
-    }
-
     @DynamicPropertySource
-    static void overrideProps(DynamicPropertyRegistry registry) {
+    static void setDatasourceProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
         registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
         registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
     }
 
-    @PreDestroy
-    public void stop() {
-        if (MYSQL_CONTAINER.isRunning()) {
-            MYSQL_CONTAINER.stop();
-        }
+    @BeforeEach
+    void cleanUp() {
+        new DbCleaner().execute();
     }
 }
